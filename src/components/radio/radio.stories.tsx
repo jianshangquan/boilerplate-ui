ab2df356d69e40f59ee1cc898ea241c2be9102f5
简尚全 <jianshangquan3@gmail.com>
2024-04-03 00:14:07 +0630

    update storybook

---
+++ src/components/radio/radio.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import "../../style.css";
+
+import React, { useState } from 'react';
+import { Radio, RadioOption } from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Radio> = {
+  title: "components/Radio",
+  component: Radio,
+  id: 'radio'
+};
+
+export default meta;
+type Story = StoryObj<typeof Radio>;
+
+export const FirstStory: Story = {
+  name: 'radio',
+  args: {
+    //👇 The args you need here will depend on your component
+  },
+  render() {
+    return React.createElement(() => {
+      const [value, setValue] = useState();
+      return (
+        <div className="w-full h-[100vh] flex items-center justify-center">
+          <Radio value={value} onChanged={(index, value) => setValue(value)} className="gap-3">
+            <RadioOption value={1} label={'Option 1'}/>
+            <RadioOption value={2} label={'Option 2'}/>
+          </Radio>
+        </div>
+      )
+    })
+  }
+};
