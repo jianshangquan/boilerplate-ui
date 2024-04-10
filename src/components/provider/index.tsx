e018b3062590829ad93c66d70abee9f0b0ef02a1
简尚全 <jianshangquan3@gmail.com>
2024-04-10 14:45:21 +0630

    add provider

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -1,6 +1,9 @@
+'use client';
 
-import React, { useContext, createContext, ReactNode } from "react";
+
+import React, { createContext, ReactNode } from "react";
 import { DefaultBoilerplateAppearance } from '../../types/appearance';
+import { AnimatedCounter } from "../animated-counter";
 
 
 
@@ -11,7 +14,61 @@ export interface BoilerplateUIProps{
 
 
 const DefaultBoilerplateUIConfig = { 
-    appearance: DefaultBoilerplateAppearance 
+    appearance: DefaultBoilerplateAppearance,
+    input: {
+        appearance: DefaultBoilerplateAppearance
+    },
+    labelTabSwitch: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    animatedCounter: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    button: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    calendar: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    checkbox: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    circularProgress: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    divider: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    githubCommitHistory: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    myanmarNRCInput: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    optionSelect: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    phoneInput: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    radio: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    select: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    slider: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    switch: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    textarea: {
+        appearance: DefaultBoilerplateAppearance,
+    },
+    textButton: {
+        appearance: DefaultBoilerplateAppearance,
+    },
 }
 
 const BoilerplateUIContext = createContext<BoilerplateUIProps>(DefaultBoilerplateUIConfig)
